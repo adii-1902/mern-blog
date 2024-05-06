de48bc677a53063fd0c979b6eced74cf0328822b
adii-1902 <kumbharaditya9903@gmail.com>
2024-05-07 00:06:37 +0530

    create the header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,15 @@
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  // eslint-disable-next-line no-undef
+  plugins: [flowbite.plugin(),],
+};
