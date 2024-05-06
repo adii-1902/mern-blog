458892fcf9edacd97c5cf6223c210c3aa2556d1d
adii-1902 <kumbharaditya9903@gmail.com>
2024-05-06 21:48:34 +0530

    instal react js and tailwind css and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-pink-700 text-3xl'>Hello</h1>
+    </div>
+  )
+}
