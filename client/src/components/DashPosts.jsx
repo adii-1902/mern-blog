e70b89c249c327aec9b647e05b4442a68d6fcf5c
adii-1902 <kumbharaditya9903@gmail.com>
2024-07-09 15:48:32 +0530

    add show more functionality to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,4 +1,4 @@
-import { Table } from 'flowbite-react';
+import { Button, Table } from 'flowbite-react';
 import React, { useState } from 'react'
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 export default function DashPosts() {
     const { currentUser } = useSelector((state) => state.user);
     const [userPosts, setUserPosts] = useState([]);
+    const [showMore, setShowMore] = useState(true);
     console.log(userPosts);
     useEffect(() => {
         const fetchPosts = async () => {
@@ -17,6 +18,9 @@ export default function DashPosts() {
                 // console.log(data);
                 if (res.ok) {
                     setUserPosts(data.posts);
+                    if (data.posts.length < 9) {
+                        setShowMore(false);
+                    }
                 }
             } catch (error) {
                 console.log(error.message);
@@ -26,11 +30,26 @@ export default function DashPosts() {
             fetchPosts();
         }
     }, [currentUser._id]);
+    const handleShowMore = async () => {
+        const startIndex = userPosts.length;
+        try {
+            const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+            const data = await res.json();
+            if (res.ok) {
+                setUserPosts((prev) => [...prev, ...data.posts]);
+                if (data.posts.length < 9) {
+                    setShowMore(false);
+                }
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
     return (
-        <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+        <div className='w-full overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
             {currentUser.isAdmin && userPosts.length > 0 ? (
                 <>
-                    <Table hoverable className='shadow-md'>
+                    <Table hoverable className='w-full min-w-full shadow-md'>
                         <Table.Head>
                             <Table.HeadCell>Date updated</Table.HeadCell>
                             <Table.HeadCell>post image</Table.HeadCell>
@@ -77,6 +96,13 @@ export default function DashPosts() {
                             </Table.Body>
                         ))}
                     </Table>
+                    {
+                        showMore && (
+                            <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+                                Show more
+                            </button>
+                        )
+                    }
                 </>
             ) :
                 <p>You have no posts yet!</p>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -16,16 +16,16 @@ export default function Dashboard() {
             setTab(tabFromUrl);
         }
     }, [location.search]);
-    return (<div className='min-h-screen flex flex-col md:flex-row'>
-        <div className='md:w-56'>
-            {/* Sidebar */}
-            <DashSidebar />
-        </div>
-        <div className="h-screen w-full">
+    return (
+        <div className='min-h-screen flex flex-col md:flex-row'>
+            <div className='md:w-56'>
+                {/* Sidebar */}
+                <DashSidebar />
+            </div>
             {/* Profile ... */}
             {tab === 'profile' && <DashProfile />}
+            {/* Posts ... */}
             {tab === 'posts' && <DashPosts />}
         </div>
-    </div>
     );
 }
