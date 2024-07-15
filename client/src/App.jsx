55a4cc8632ab9ad0c3a1bcf01a876fda5576d92f
adii-1902 <kumbharaditya9903@gmail.com>
2024-07-15 18:32:52 +0530

    complete search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/UpdatePost'
 import PostPage from './pages/PostPage'
 import ScrollToTop from './components/ScrollToTop'
+import Search from './components/Search'
 
 
 export default function App() {
@@ -28,6 +29,7 @@ export default function App() {
         <Route path="/about" element={<About />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/search" element={<Search />}></Route>
         <Route element={<PrivateRoute />} >
           <Route path="/dashboard" element={<Dashboard />}></Route>
         </Route>
